020b8744a8928e0bc5fd1c2854c404bfd14c5696
Jose F Rivas C <josefelixrc7@gmail.com>
2024-09-30 12:44:02 -0400

    feat: Add Read_ to FormsData definition

--- src/functions/forms.cpp
+++ src/functions/forms.cpp
@@ -1,9 +1,137 @@
 
 #include "functions/forms.h"
-#include <algorithm>
+
+FormsData::FormsData(FunctionData& function_data) :
+    FunctionData(function_data)
+{
+    Read_();
+}
+
+void FormsData::Read_()
+{
+    // Function GET /api/forms/data/read
+    Functions::Function::Ptr function = 
+        std::make_shared<Functions::Function>("/api/forms/data/read", HTTP::EnumMethods::kHTTP_GET);
+
+    function->set_response_type(Functions::Function::ResponseType::kCustom);
+
+    // Action 1: Get current identifier and id_space
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code("SELECT identifier, id_space FROM forms WHERE identifier = ? AND id_space = ?");
+    action1->set_final(false);
+    action1->AddParameter_("identifier", Tools::DValue(""), true)
+    ->SetupCondition_("condition-identifier", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->get_value().ToString_() == "")
+        {
+            param->set_error("El identificador no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+
+    action1->AddParameter_("id_space", Tools::DValue(get_space_id()), false);
+
+    // Action 2: Get form columns
+    auto action2 = function->AddAction_("a2");
+    action2->set_sql_code(
+        "SELECT fc.identifier, fc.name " \
+        "FROM forms_columns fc " \
+        "JOIN forms f ON f.id = fc.id_form " \
+        "WHERE f.identifier = ? AND f.id_space = ?"
+    );
+    action2->set_final(false);
+    action2->AddParameter_("identifier", Tools::DValue(""), true)
+    ->SetupCondition_("condition-identifier", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->get_value().ToString_() == "")
+        {
+            param->set_error("El identificador no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+
+    action2->AddParameter_("id_space", Tools::DValue(get_space_id()), false);
+
+    // Setup Custom Process
+    function->SetupCustomProcess_([&](Functions::Function& self)
+    {
+        // Search first action
+        if(self.get_actions().begin() == self.get_actions().end())
+        {
+            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error FkwTVZabIw");
+            return;
+        }
+
+        // Iterate over actions
+        for(auto action : self.get_actions())
+        {
+            // Execute action
+            if(!action->Work_())
+            {
+                self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error " + action->get_identifier() + ": MS46GLPi6D");
+                return;
+            }
+        }
+
+        // Get actions
+        auto action1 = self.GetAction_("a1");
+        auto action2 = self.GetAction_("a2");
+        if(action1 == self.get_actions().end() || action2 == self.get_actions().end())
+        {
+            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error QFt5mE0RfV");
+            return;
+        }
+
+        // Get form info
+        auto identifier = action1->get()->get_results()->ExtractField_(0, 0);
+        auto id_space = action1->get()->get_results()->ExtractField_(0, 1);
+
+        if(identifier->IsNull_() || id_space->IsNull_())
+        {
+            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error HPqWlZkEbk");
+            return;
+        }
+
+        // Get columns
+        std::string columns = "";
+        for(auto it : *action2->get()->get_results())
+        {
+            auto identifier = it.get()->ExtractField_("identifier");
+            auto name = it.get()->ExtractField_("name");
+            if(identifier->IsNull_() || name->IsNull_())
+                continue;
+
+            if(it == *action2->get()->get_results()->begin())
+                columns = identifier->ToString_() + " AS '" + name->ToString_() + "'";
+            else
+                columns += ", " + identifier->ToString_() + " AS '" + name->ToString_() + "'";
+        }
+
+        // Action 3: Get Form data
+            Functions::Action action3("a3");
+            action3.set_sql_code(
+                "SELECT id, " + columns + " " \
+                "FROM form_" + id_space->ToString_() + "_" + identifier->ToString_());
+            if(!action3.Work_())
+            {
+                self.JSONResponse_(HTTP::Status::kHTTP_INTERNAL_SERVER_ERROR, "Error UgOMMObhM2");
+                return;
+            }
+
+            auto json_result = action3.CreateJSONResult_();
+            json_result->set("status", action3.get_status());
+            json_result->set("message", action3.get_message());
+            self.CompoundResponse_(HTTP::Status::kHTTP_OK, json_result);
+    });
+
+    get_functions()->push_back(function);
+}
 
 Forms::Forms(FunctionData& function_data) :
     FunctionData(function_data)
+    ,forms_data_(function_data)
 {
     Read_();
     ReadSpecific_();
