87b3d802862c3f3d6ad792b88d85eaa645a34d3e
Jose F Rivas C <josefelixrc7@gmail.com>
2024-10-18 22:47:15 -0400

    feat: Use Modify ActionsData in FormsData

--- src/functions/forms/forms_data.cpp
+++ src/functions/forms/forms_data.cpp
@@ -359,92 +359,43 @@ void StructBI::Functions::FormsData::Modify_()
 
     // Action 1: Verify form existence
     auto action1 = function->AddAction_("a1");
-    action1->set_sql_code("SELECT identifier, id_space FROM forms WHERE identifier = ? AND id_space = ?");
-    action1->set_final(false);
-    action1->SetupCondition_("verify-form-existence", Query::ConditionType::kError, [](NAF::Functions::Action& self)
-    {
-        if(self.get_results()->size() != 1)
-        {
-            self.set_custom_error("El formulario solicitado no existe");
-            return false;
-        }
-
-        return true;
-    });
-
-    action1->AddParameter_("form-identifier", "", true)
-    ->SetupCondition_("condition-identifier-form", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
-    {
-        if(param->get_value()->ToString_() == "")
-        {
-            param->set_error("El identificador de formulario no puede estar vacío");
-            return false;
-        }
-        return true;
-    });
-
-    action1->AddParameter_("id_space", get_space_id(), false);
+    actions_.forms_data_.modify_01_.Setup_(action1);
 
     // Action 2: Get form columns
     auto action2 = function->AddAction_("a2");
-    action2->set_sql_code(
-        "SELECT fc.*, fct.identifier AS column_type " \
-        "FROM forms_columns fc " \
-        "JOIN forms_columns_types fct ON fct.id = fc.id_column_type " \
-        "JOIN forms f ON f.id = fc.id_form " \
-        "WHERE f.identifier = ? AND f.id_space = ?"
-    );
-    action2->set_final(false);
-    action2->AddParameter_("form-identifier", "", true)
-    ->SetupCondition_("condition-form-identifier", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
-    {
-        if(param->get_value()->ToString_() == "")
-        {
-            param->set_error("El identificador de formulario no puede estar vacío");
-            return false;
-        }
-        return true;
-    });
+    actions_.forms_data_.modify_02_.Setup_(action2);
 
-    action2->AddParameter_("id_space", get_space_id(), false);
+    // Action 3: Save new record
+    auto action3 = function->AddAction_("a3");
+    actions_.forms_data_.modify_03_.Setup_(action3);
 
     // Setup Custom Process
     auto id_space = get_space_id();
-    function->SetupCustomProcess_([id_space](NAF::Functions::Function& self)
+    function->SetupCustomProcess_([id_space, action1, action2, action3](NAF::Functions::Function& self)
     {
-        // Verify if exists actions
-        if(self.get_actions().begin() == self.get_actions().end())
+        // Execute actions
+        if(!action1->Work_())
         {
-            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error fpnd6GDTxF");
+            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error " + action1->get_identifier() + ": CnMsvrA4aa");
             return;
         }
-
-        // Execute actions
-        for(auto action : self.get_actions())
+        if(!action2->Work_())
         {
-            if(!action->Work_())
-            {
-                self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error " + action->get_identifier() + ": CnMsvrA4aa");
-                return;
-            }
+            self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error " + action2->get_identifier() + ": Fr5MHxX1wQ");
+            return;
         }
 
         // Get form info
         auto form_identifier = self.GetParameter_("form-identifier");
-
         if(form_identifier == self.get_parameters().end())
         {
             self.JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Error 1VxAfjpkhN");
             return;
         }
 
-        // Action 3: Save new record
-        auto action3 = self.AddAction_("a3");
-
         // Get columns from action 2
-        auto action2 = self.GetAction_("a2");
         std::string columns = "";
-        for(auto it : *action2->get()->get_results())
+        for(auto it : *action2->get_results())
         {
             // Get column
             auto identifier = it.get()->ExtractField_("identifier");
