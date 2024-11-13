97410cbde121605402d0c847c9f33a2717ca1d73
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-13 10:44:07 -0400

    feat: Add function to Read organizations

--- src/functions/organizations.cpp
+++ src/functions/organizations.cpp
@@ -12,5 +12,18 @@ Organizations::Organizations(Tools::FunctionData& function_data) :
 
 void Organizations::Read_()
 {
+    // Function GET /api/organizations/read
+    NAF::Functions::Function::Ptr function = 
+        std::make_shared<NAF::Functions::Function>("/api/organizations/read", HTTP::EnumMethods::kHTTP_GET);
     
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code(
+        "SELECT * \
+        FROM organizations o \
+        JOIN organizations_users ou ON ou.id_organization = o.id \
+        WHERE ou.id_naf_user = ?"
+    );
+    action1->AddParameter_("id_user", get_id_user(), false);
+    
+    get_functions()->push_back(function);
 }
