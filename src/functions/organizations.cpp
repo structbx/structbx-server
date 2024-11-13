819d2bdb7d21f3c0dbc41fff0fe1c45373a047d0
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-13 10:22:49 -0400

    feat: Add Organizations class

---
+++ src/functions/organizations.cpp
@@ -0,0 +1,16 @@
+
+#include "functions/organizations.h"
+
+using namespace StructBI::Functions;
+
+Organizations::Organizations(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+    ,actions_(function_data)
+{
+    Read_();
+}
+
+void Organizations::Read_()
+{
+    
+}
