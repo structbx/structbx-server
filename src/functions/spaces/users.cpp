17315e5c25dc25353083f448a42a2c95beb119a8
Jose F Rivas C <josefelixrc7@gmail.com>
2025-01-29 21:46:31 -0400

    feat: Remove use of organization tables
    Rel: #21

--- src/functions/spaces/users.cpp
+++ src/functions/spaces/users.cpp
@@ -52,13 +52,11 @@ Users::ReadUserOutSpace::ReadUserOutSpace(Tools::FunctionData& function_data) :
     action1->set_sql_code(
         "SELECT nu.id, nu.username "
         "FROM _naf_users nu "
-        "JOIN organizations_users ou ON ou.id_naf_user = nu.id "
         "LEFT JOIN spaces_users su ON "
             "su.id_naf_user = nu.id AND "
             "su.id_space = (SELECT s.id FROM spaces s JOIN spaces_users su2 ON su2.id_space = s.id WHERE identifier = ? AND su2.id_naf_user = ? LIMIT 1) "
         "WHERE "
-            "ou.id_organization = (SELECT id_organization FROM organizations_users WHERE id_naf_user = ?) "
-            "AND su.id_naf_user IS NULL "
+            "su.id_naf_user IS NULL "
     );
     action1->AddParameter_("identifier_space", "", true)
     ->SetupCondition_("condition-identifier_space", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
@@ -71,7 +69,6 @@ Users::ReadUserOutSpace::ReadUserOutSpace(Tools::FunctionData& function_data) :
         return true;
     });
     action1->AddParameter_("id_user", get_id_user(), false);
-    action1->AddParameter_("id_user2", get_id_user(), false);
 
     get_functions()->push_back(function);
 }
