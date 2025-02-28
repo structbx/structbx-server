93460bf2077b6e18150745a0d069c48ad858b6e9
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-28 10:59:36 -0400

    feat: Add Tools::DValue class
    Rel: #37

---
+++ src/tools/dvalue.cpp
@@ -0,0 +1,423 @@
+/*
+ * Nebula Atom
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "tools/dvalue.h"
+
+using namespace NAF::Tools;
+
+DValue::DValue() :
+    type_(Type::kEmpty)
+    ,value_string_("")
+    ,value_int_(0)
+    ,value_float_(0.f)
+    ,value_bool_(false)
+{
+    
+}
+
+DValue::DValue(Poco::Dynamic::Var& value) :
+    type_(DValue::Type::kString)
+    ,value_string_("")
+    ,value_int_(0)
+    ,value_float_(0.f)
+    ,value_bool_(false)
+{
+    Format_(value);
+}
+
+DValue::DValue(std::string value_string) :
+    type_(DValue::Type::kString)
+    ,value_string_(value_string)
+    ,value_int_(0)
+    ,value_float_(0.f)
+    ,value_bool_(false)
+{
+    
+}
+
+DValue::DValue(const char* value_string) :
+    type_(DValue::Type::kString)
+    ,value_string_(value_string)
+    ,value_int_(0)
+    ,value_float_(0.f)
+    ,value_bool_(false)
+{
+    
+}
+
+DValue::DValue(int value_int) :
+    type_(DValue::Type::kInteger)
+    ,value_string_("")
+    ,value_int_(value_int)
+    ,value_float_(0.f)
+    ,value_bool_(false)
+{
+    
+}
+
+DValue::DValue(float value_float) :
+    type_(DValue::Type::kFloat)
+    ,value_string_("")
+    ,value_int_(0)
+    ,value_float_(value_float)
+    ,value_bool_(false)
+{
+    
+}
+
+DValue::DValue(bool value_bool) :
+    type_(DValue::Type::kBoolean)
+    ,value_string_("")
+    ,value_int_(0)
+    ,value_float_(0.f)
+    ,value_bool_(value_bool)
+{
+    
+}
+
+bool DValue::operator==(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ == dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ == dvalue.get_value_float();
+            case Type::kInteger: return value_int_ == dvalue.get_value_int();
+            case Type::kString: return value_string_ == dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::operator!=(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ != dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ != dvalue.get_value_float();
+            case Type::kInteger: return value_int_ != dvalue.get_value_int();
+            case Type::kString: return value_string_ != dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::operator<(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ < dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ < dvalue.get_value_float();
+            case Type::kInteger: return value_int_ < dvalue.get_value_int();
+            case Type::kString: return value_string_ < dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::operator<=(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ <= dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ <= dvalue.get_value_float();
+            case Type::kInteger: return value_int_ <= dvalue.get_value_int();
+            case Type::kString: return value_string_ <= dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::operator>(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ > dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ > dvalue.get_value_float();
+            case Type::kInteger: return value_int_ > dvalue.get_value_int();
+            case Type::kString: return value_string_ > dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::operator>=(DValue& dvalue)
+{
+    try
+    {
+        if(!TypeIsIqual_(dvalue.type_))
+        {
+            throw std::runtime_error("Error when comparing different types of data.");
+            return false;
+        }
+
+        switch(dvalue.type_)
+        {
+            case Type::kEmpty: return true;
+            case Type::kBoolean: return value_bool_ >= dvalue.get_value_bool();
+            case Type::kFloat: return value_float_ >= dvalue.get_value_float();
+            case Type::kInteger: return value_int_ >= dvalue.get_value_int();
+            case Type::kString: return value_string_ >= dvalue.get_value_string();
+        }
+    }
+    catch(std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on dvalue.cpp on operator==(): " + std::string(error.what()));
+        return false;
+    }
+
+    return false;
+}
+
+bool DValue::TypeIsIqual_(DValue::Type row_value_type)
+{
+    return row_value_type == type_;
+}
+
+std::string DValue::ToString_()
+{
+    std::string final_string = "";
+    switch(type_)
+    {
+        case Type::kEmpty:
+        {
+            try
+            {
+                throw std::runtime_error("Error to convert Empty value to String.");
+            }
+            catch(std::exception& error)
+            {
+                Tools::OutputLogger::Error_("Error on dvalue.cpp on ToString_(): " + std::string(error.what()));
+            }
+            break;
+        }
+        case Type::kBoolean:
+        {
+            try
+            {
+                final_string = std::to_string(value_bool_);
+            }
+            catch(std::exception& error)
+            {
+                Tools::OutputLogger::Error_("Error on dvalue.cpp on ToString_(): " + std::string(error.what()));
+            }
+            break;
+        }
+        case Type::kFloat:
+        {
+            try
+            {
+                final_string = std::to_string(value_float_);
+            }
+            catch(std::exception& error)
+            {
+                Tools::OutputLogger::Error_("Error on dvalue.cpp on ToString_(): " + std::string(error.what()));
+            }
+            break;
+        }
+        case Type::kInteger:
+        {
+            try
+            {
+                final_string = std::to_string(value_int_);
+            }
+            catch(std::exception& error)
+            {
+                Tools::OutputLogger::Error_("Error on dvalue.cpp on ToString_(): " + std::string(error.what()));
+            }
+            break;
+        }
+        case Type::kString:
+        {
+            try
+            {
+                final_string = value_string_;
+            }
+            catch(std::exception& error)
+            {
+                Tools::OutputLogger::Error_("Error on dvalue.cpp on ToString_(): " + std::string(error.what()));
+            }
+            break;
+        }
+    }
+
+    return final_string;
+}
+
+void DValue::Format_(Poco::Dynamic::Var& value)
+{
+    if(value.isEmpty())
+        type_ = DValue::Type::kEmpty;
+    else if(value.isNumeric())
+        if(value.isInteger())
+        {
+            if(value.isBoolean())
+            {
+                value_bool_ = value.convert<bool>();
+                type_ = DValue::Type::kBoolean;
+            }
+            else
+            {
+                value_int_ = std::stoi(value.toString());
+                type_ = DValue::Type::kInteger;
+            }
+        }
+        else
+        {
+            value_float_ = std::stof(value.toString());
+            type_ = DValue::Type::kFloat;
+        }
+    else if(value.isDate() || value.isDateTime())
+    {
+        DateTime date;
+        int diff;
+
+        if(DateTimeParser::tryParse(DateTimeFormat::ISO8601_FORMAT, value.toString(), date, diff))
+        {
+            DateTimeParser::parse(DateTimeFormat::ISO8601_FORMAT, value.toString(), date, diff);
+            auto date_string = DateTimeFormatter::format(date, DateTimeFormat::SORTABLE_FORMAT);
+
+            value_string_ = date_string;
+            type_ = DValue::Type::kString;
+        }
+        else
+        {
+            value_string_ = value.toString();
+            type_ = DValue::Type::kString;
+        }
+    }
+    else
+    {
+        value_string_ = value.toString();
+        type_ = DValue::Type::kString;
+    }
+}
+
+std::string& DValue::String_()
+{
+    if(!TypeIsIqual_(Tools::DValue::Type::kString))
+    {
+        throw std::runtime_error("Error on dvalue.cpp on String_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_string_;
+}
+
+float& DValue::Float_()
+{
+    if(!TypeIsIqual_(Tools::DValue::Type::kFloat))
+    {
+        throw std::runtime_error("Error on dvalue.cpp on Float_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_float_;
+}
+
+bool& DValue::Bool_()
+{
+    if(!TypeIsIqual_(Tools::DValue::Type::kBoolean))
+    {
+        throw std::runtime_error("Error on dvalue.cpp on String_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_bool_;
+}
+
+int& DValue::Int_()
+{
+    if(!TypeIsIqual_(Tools::DValue::Type::kInteger))
+    {
+        throw std::runtime_error("Error on dvalue.cpp on String_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_int_;
+}
---
+++ src/tools/dvalue.h
@@ -0,0 +1,124 @@
+/*
+ * Nebula Atom
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef NAF_TOOLS_DVALUE
+#define NAF_TOOLS_DVALUE
+
+
+#include <string>
+#include <stdexcept>
+
+#include <Poco/Dynamic/Var.h>
+#include "Poco/DateTimeParser.h"
+#include "Poco/DateTime.h"
+#include "Poco/DateTimeFormat.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/Timestamp.h"
+
+#include "tools/output_logger.h"
+
+
+namespace NAF
+{
+    namespace Tools
+    {
+        class DValue;
+    }
+}
+
+using namespace Poco;
+
+
+class NAF::Tools::DValue
+{
+    public:
+        using Ptr = std::shared_ptr<DValue>;
+
+        enum class Type
+        {
+            kEmpty
+            ,kString
+            ,kInteger
+            ,kFloat
+            ,kBoolean
+        };
+
+        DValue();
+        DValue(Poco::Dynamic::Var& value);
+        DValue(std::string value_string);
+        DValue(const char* value_string);
+        DValue(int value_int);
+        DValue(float value_float);
+        DValue(bool value_bool);
+        ~DValue(){}
+
+        Type& get_type()
+        {
+            auto& var = type_;
+            return var;
+        }
+
+        bool operator==(DValue& dvalue);
+        bool operator!=(DValue& dvalue);
+        bool operator<(DValue& dvalue);
+        bool operator<=(DValue& dvalue);
+        bool operator>(DValue& dvalue);
+        bool operator>=(DValue& dvalue);
+        bool TypeIsIqual_(Type row_value_type);
+        std::string ToString_();
+        std::string& String_();
+        float& Float_();
+        bool& Bool_();
+        int& Int_();
+
+    protected:
+        std::string& get_value_string()
+        {
+            auto& var = value_string_;
+            return var;
+        }
+        int& get_value_int()
+        {
+            auto& var = value_int_;
+            return var;
+        }
+        float& get_value_float()
+        {
+            auto& var = value_float_;
+            return var;
+        }
+        bool& get_value_bool()
+        {
+            auto& var = value_bool_;
+            return var;
+        }
+
+        void Format_(Poco::Dynamic::Var& value);
+
+    private:
+        Type type_;
+
+        std::string value_string_;
+        int value_int_;
+        float value_float_;
+        bool value_bool_;
+
+};
+
+
+#endif // NAF_TOOLS_DVALUE
