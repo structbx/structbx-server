edf134caf1331f9c946c5554e4b2ceca82781611
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-28 10:58:28 -0400

    feat: Add Security::SecurityVerification class
    Rel: #37

---
+++ src/security/security_verification.cpp
@@ -0,0 +1,60 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "security/security_verification.h"
+
+using namespace NAF::Extras;
+
+SecurityVerification::SecurityVerification() :
+    security_type_(SecurityType::kDisableAll)
+{
+
+}
+
+bool SecurityVerification::VerifyRoutesPermissions_(Tools::Route& route, std::string action_type)
+{
+    try
+    {
+        auto p = permissions_manager_.FindPermission_(route, users_manager_.get_current_user().get_id(), action_type);
+
+        // Permission not found
+        switch(security_type_)
+        {
+            case SecurityType::kDisableAll:
+            {
+                if(p == permissions_manager_.get_permissions().end())
+                    return false;
+
+                break;
+            }
+            case SecurityType::kEnableAll:
+            {
+                if(p == permissions_manager_.get_permissions().end())
+                    return true;
+                
+                break;
+            }
+        }
+    }
+    catch(const std::exception& error)
+    {
+        Tools::OutputLogger::Error_("Error on security_verification.cpp on VerifyRoutesPermissions_(): " + std::string(error.what()));
+        return false;
+    }
+    return true;
+}
