a4eedf268bc0b4561e9aeeed097fff3887a7809b
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-28 10:57:21 -0400

    feat: Add Query::Parameter class
    Rel: #37

---
+++ src/query/parameter.cpp
@@ -0,0 +1,78 @@
+
+#include "query/parameter.h"
+
+using namespace NAF::Query;
+
+Parameter::Parameter(std::string name, Tools::DValue::Ptr value, bool editable) :
+    parameter_type_(ParameterType::kField)
+    ,name_(name)
+    ,error_("")
+    ,editable_(editable)
+    ,value_(value)
+    ,field_position_(0, 0)
+    ,related_action_("")
+{
+    
+}
+
+Parameter::Parameter(std::string name, Query::Field::Position field_position, std::string related_action, bool editable) :
+    parameter_type_(ParameterType::kPosition)
+    ,name_(name)
+    ,error_("")
+    ,editable_(editable)
+    ,value_(new Tools::DValue())
+    ,field_position_(field_position)
+    ,related_action_(related_action)
+{
+    
+}
+
+std::string Parameter::ToString_()
+{
+    return value_->ToString_();
+}
+
+std::string& Parameter::StringValue_()
+{
+    if(!value_->TypeIsIqual_(Tools::DValue::Type::kString))
+    {
+        throw std::runtime_error("Error on Parameter.cpp on StringValue_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_->String_();
+}
+
+int& Parameter::IntValue_()
+{
+    if(!value_->TypeIsIqual_(Tools::DValue::Type::kInteger))
+    {
+        throw std::runtime_error("Error on Parameter.cpp on IntValue_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_->Int_();
+}
+
+bool& Parameter::BoolValue_()
+{
+    if(!value_->TypeIsIqual_(Tools::DValue::Type::kBoolean))
+    {
+        throw std::runtime_error("Error on Parameter.cpp on BoolValue_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_->Bool_();
+}
+
+float& Parameter::FloatValue_()
+{
+    if(!value_->TypeIsIqual_(Tools::DValue::Type::kFloat))
+    {
+        throw std::runtime_error("Error on Parameter.cpp on FloatValue_(): The data type to be obtained does not match the current data type.");
+    }
+
+    return value_->Float_();
+}
+
+void Parameter::SetupCondition_(std::string identifier, Query::ConditionType type, Query::Condition<Ptr>::Functor functor)
+{
+    condition_ = std::make_shared<Query::Condition<Ptr>>(identifier, type, functor);
+}
---
+++ src/query/parameter.h
@@ -0,0 +1,101 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2023  Jose F Rivas C <josefelixrc7@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NAF_QUERY_PARAMETER
+#define NAF_QUERY_PARAMETER
+
+
+#include <string>
+
+#include "tools/dvalue.h"
+#include "query/results.h"
+#include "query/condition.h"
+
+
+namespace NAF
+{
+    namespace Query
+    {
+        enum class ParameterType;
+        class Parameter;
+    }
+}
+
+
+enum class NAF::Query::ParameterType
+{
+    kField
+    ,kPosition
+};
+
+class NAF::Query::Parameter
+{
+    public:
+        using Ptr = std::shared_ptr<Parameter>;
+        using Vector = std::vector<Ptr>;
+
+        Parameter(std::string name, Tools::DValue::Ptr value, bool editable);
+        Parameter(std::string name, Query::Field::Position field_position, std::string related_action, bool editable);
+
+        ParameterType get_parameter_type() const { return parameter_type_; }
+        std::string get_name() const { return name_; }
+        std::string get_error() const { return error_; }
+        bool get_editable() const { return editable_; }
+        Tools::DValue::Ptr& get_value()
+        {
+            auto& var = value_;
+            return var;
+        }
+        Query::Field::Position& get_field_position()
+        {
+            auto& var = field_position_;
+            return var;
+        }
+        std::string get_related_action() const { return related_action_; }
+        Query::Condition<Ptr>::Ptr& get_condition()
+        {
+            auto& var = condition_;
+            return var;
+        }
+
+        void set_parameter_type(ParameterType parameter_type) { parameter_type_ = parameter_type; }
+        void set_name(std::string name) { name_ = name; }
+        void set_error(std::string error) { error_ = error; }
+        void set_editable(bool editable) { editable_ = editable; }
+        void set_value(Tools::DValue::Ptr value) { value_ = value; }
+        void set_related_action(std::string related_action) { related_action_ = related_action; }
+
+        std::string ToString_();
+        std::string& StringValue_();
+        int& IntValue_();
+        bool& BoolValue_();
+        float& FloatValue_();
+        void SetupCondition_(std::string identifier, Query::ConditionType type, Query::Condition<Ptr>::Functor functor);
+
+    private:
+        ParameterType parameter_type_;
+        std::string name_;
+        std::string error_;
+        bool editable_;
+        Tools::DValue::Ptr value_;
+        Query::Field::Position field_position_;
+        std::string related_action_;
+        Query::Condition<Ptr>::Ptr condition_;
+};
+
+#endif // NAF_QUERY_PARAMETER
