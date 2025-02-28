500d36ac1e78b62f4c9d7e9cf419fc0751b3233f
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-28 10:56:40 -0400

    feat: Add HTTP::Request class
    Rel: #37

---
+++ src/http/request.cpp
@@ -0,0 +1,79 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "http/request.h"
+
+using namespace NAF::HTTP;
+
+Request::Request()
+{
+    http_server_request_ = std::nullopt;
+    http_server_response_ = std::nullopt;
+}
+
+void Request::AddHeader_(std::string name, std::string value)
+{
+    headers_.push_back(Header(name, value));
+}
+
+void Request::AddHeader_(HTTP::Header header)
+{
+    headers_.push_back(header);
+}
+
+void Request::AddCookie_(std::string name, std::string value)
+{
+    cookies_.push_back(Cookie(name, value));
+}
+
+void Request::AddCookie_(HTTP::Cookie cookie)
+{
+    cookies_.push_back(cookie);
+}
+
+void Request::SetupRequest_(Net::HTTPServerRequest& request)
+{
+    http_server_request_.emplace(&request);
+}
+
+void Request::SetupResponse_(Net::HTTPServerResponse& response)
+{
+    http_server_response_.emplace(&response);
+}
+
+void Request::SetupHeaders_()
+{
+    for(auto& header : headers_)
+        http_server_response_.value()->add(header.name, header.value);
+}
+
+void Request::SetupCookies_()
+{
+    for(auto& cookie : cookies_)
+    {
+        if(cookie.name == "")
+        {
+            http_server_response_.value()->addCookie(cookie.cookie);
+        }
+        else
+        {
+            HTTPCookie poco_cookie(cookie.name, cookie.value);
+            http_server_response_.value()->addCookie(poco_cookie);
+        }
+    }
+}
---
+++ src/http/request.h
@@ -0,0 +1,126 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef NAF_HTTP_REQUEST
+#define NAF_HTTP_REQUEST
+
+
+#include <vector>
+#include <optional>
+
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/HTTPCookie.h>
+#include <Poco/Net/NameValueCollection.h>
+
+
+namespace NAF
+{
+    namespace HTTP
+    {
+        class Header;
+        class Cookie;
+        class Request;
+    }
+}
+
+using namespace Poco;
+using namespace Poco::Net;
+
+
+
+class NAF::HTTP::Header
+{
+    public:
+        Header(std::string name, std::string value) : 
+            name(name)
+            ,value(value)
+        {}
+        virtual ~Header(){}
+
+        std::string name;
+        std::string value;
+};
+
+class NAF::HTTP::Cookie
+{
+    public:
+        Cookie(std::string name, std::string value) : 
+            name(name)
+            ,value(value)
+        {}
+        Cookie(Net::HTTPCookie cookie) : 
+            name("")
+            ,value("")
+            ,cookie(cookie)
+        {}
+        virtual ~Cookie(){}
+
+        std::string name;
+        std::string value;
+        Net::HTTPCookie cookie;
+};
+
+class NAF::HTTP::Request
+{
+    public:
+        using HTTPServerRequestPtr = std::optional<HTTPServerRequest*>;
+        using HTTPServerResponsePtr = std::optional<HTTPServerResponse*>;
+
+        Request();
+        
+        HTTPServerRequestPtr& get_http_server_request()
+        {
+            auto& var = http_server_request_;
+            return var;
+        }
+        HTTPServerResponsePtr& get_http_server_response()
+        {
+            auto& var = http_server_response_;
+            return var;
+        }
+        std::vector<HTTP::Header> get_headers()
+        {
+            auto& var = headers_;
+            return var;
+        }
+        std::vector<HTTP::Cookie> get_cookies()
+        {
+            auto& var = cookies_;
+            return var;
+        }
+        
+        void AddHeader_(std::string name, std::string value);
+        void AddHeader_(HTTP::Header header);
+        void AddCookie_(std::string name, std::string value);
+        void AddCookie_(HTTP::Cookie cookie);
+
+    protected:
+        void SetupRequest_(Net::HTTPServerRequest& request);
+        void SetupResponse_(Net::HTTPServerResponse& response);
+        void SetupHeaders_();
+        void SetupCookies_();
+
+    private:
+        HTTPServerRequestPtr http_server_request_;
+        HTTPServerResponsePtr http_server_response_;
+        std::vector<HTTP::Header> headers_;
+        std::vector<HTTP::Cookie> cookies_;
+};
+
+#endif // NAF_HTTP_REQUEST
