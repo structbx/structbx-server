de2eef283cc74c413f7c9ff38a985d826ab5ab49
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-28 10:54:49 -0400

    feat: Add Functions::Function class
    Rel: #37

---
+++ src/functions/function.cpp
@@ -0,0 +1,419 @@
+
+#include "functions/function.h"
+
+using namespace NAF;
+using namespace NAF::Functions;
+
+Function::Function() :
+    endpoint_("")
+    ,target_("")
+    ,error_(false)
+    ,error_message_("Unknown error.")
+    ,remove_file_on_modify_(true)
+    ,response_type_(ResponseType::kJSON)
+    ,method_(HTTP::EnumMethods::kHTTP_GET)
+    ,file_manager_(new Files::FileManager())
+    ,custom_process_([](Function&){})
+{
+    
+}
+
+Function::Function(std::string endpoint, HTTP::EnumMethods method, ResponseType response_type) :
+    endpoint_(endpoint)
+    ,target_("")
+    ,error_(false)
+    ,error_message_("Unknown error.")
+    ,remove_file_on_modify_(true)
+    ,response_type_(response_type)
+    ,method_(method)
+    ,file_manager_(new Files::FileManager())
+    ,custom_process_([](Function&){})
+{
+    
+}
+
+std::vector<Query::Parameter::Ptr>::iterator Function::GetParameter_(std::string name)
+{
+    auto found = std::find_if(parameters_.begin(), parameters_.end(), [&name](Query::Parameter::Ptr parameter)
+    {
+        return parameter->get_name() == name;
+    });
+
+    return found;
+}
+
+Action::Ptr Function::AddAction_(std::string identifier)
+{
+    Action::Ptr action = std::make_shared<Action>(identifier);
+    actions_.push_back(action);
+    return action;
+}
+
+std::vector<Action::Ptr>::iterator Function::GetAction_(std::string identifier)
+{
+    auto found = std::find_if(actions_.begin(), actions_.end(), [&identifier](Action::Ptr action)
+    {
+        return action->get_identifier() == identifier;
+    });
+
+    return found;
+}
+
+void Function::Setup_(HTTP::Request::HTTPServerRequestPtr request, HTTP::Request::HTTPServerResponsePtr response)
+{
+    if(request.has_value())
+        SetupRequest_(*request.value());
+    else
+    {
+        JSONResponse_(HTTP::Status::kHTTP_INTERNAL_SERVER_ERROR, "Error to setup Request in Function");
+        return;
+    }
+    if(response.has_value())
+        SetupResponse_(*response.value());
+    else
+    {
+        JSONResponse_(HTTP::Status::kHTTP_INTERNAL_SERVER_ERROR, "Error to setup Response in Function");
+        return;
+    }
+}
+
+void Function::Process_(HTTP::Request::HTTPServerRequestPtr request, HTTP::Request::HTTPServerResponsePtr response)
+{
+    bool json_sent = false;
+
+    Tools::OutputLogger::Debug_("Function " + endpoint_);
+
+    try
+    {
+        Setup_(request, response);
+
+        switch(response_type_)
+        {
+            case Functions::Function::ResponseType::kJSON:
+            {
+                JSON::Object::Ptr json_result = new JSON::Object();
+                if(!ProcessJSON_(json_result))
+                {
+                    if(error_)
+                    {
+                        JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, error_message_);
+                        json_sent = true;
+                        throw std::runtime_error("Error on function.cpp on Process_(): " + error_message_);
+                        return;
+                    }
+                }
+
+                // Send JSON results
+                CompoundResponse_(HTTP::Status::kHTTP_OK, json_result);
+
+                break;
+            }
+            case Functions::Function::ResponseType::kFile:
+            {
+                std::string filepath = "";
+                if(!ProcessFile_(filepath))
+                {
+                    if(error_)
+                    {
+                        JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, error_message_);
+                        json_sent = true;
+                        throw std::runtime_error("Error on function.cpp on Process_(): " + error_message_);
+                        return;
+                    }
+                }
+
+                switch(method_)
+                {
+                    case HTTP::EnumMethods::kHTTP_GET:
+                        DownloadProcess_(filepath);
+                        break;
+                    case HTTP::EnumMethods::kHTTP_POST:
+                        UploadProcess_();
+                        break;
+                    case HTTP::EnumMethods::kHTTP_PUT:
+                        ModifyProcess_(filepath);
+                        break;
+                    case HTTP::EnumMethods::kHTTP_DEL:
+                        RemoveProcess_(filepath);
+                        break;
+                    case HTTP::EnumMethods::kHTTP_HEAD:
+                    case HTTP::EnumMethods::kHTTP_OPTIONS:
+                    case HTTP::EnumMethods::kHTTP_PATCH:
+                    case HTTP::EnumMethods::kNULL:
+                        JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The client provided a bad HTTP method.");
+                        json_sent = true;
+                        throw std::runtime_error("Error on function.cpp on Process_(): The client provided a bad HTTP method.");
+                        break;
+                }
+                
+                break;
+            }
+            case Functions::Function::ResponseType::kCustom:
+            {
+                custom_process_(*this);
+                break;
+            }
+        }
+    }
+    catch(std::runtime_error& error)
+    {
+        if(!json_sent)
+            JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, error_message_);
+
+        Tools::OutputLogger::Error_("Error on function.cpp on Process_(): " + error_message_);
+        return;
+    }
+    catch(std::exception& error)
+    {
+        if(!json_sent)
+            JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, error_message_);
+
+        Tools::OutputLogger::Error_("Error on function.cpp on Process_(): " + error_message_);
+        return;
+    }
+    catch(...)
+    {
+        if(!json_sent)
+            JSONResponse_(HTTP::Status::kHTTP_INTERNAL_SERVER_ERROR, "Unknown error");
+
+        Tools::OutputLogger::Error_("Error on function.cpp on Process_(): Unknown error");
+        return;
+    }
+}
+
+bool Function::ProcessAction_(Action::Ptr action)
+{
+    // Copy actions references
+    action->get_actions_container().clear();
+    action->get_actions_container().insert(action->get_actions_container().end(), actions_.begin(), actions_.end());
+    
+    // Identify parameters
+    if(action->get_error())
+    {
+        error_ = true;
+        error_message_ = action->get_custom_error();
+        return false;
+    }
+
+    // Execute action
+    if(!action->Work_())
+    {
+        error_ = true;
+        error_message_ = action->get_custom_error();
+        return false;
+    }
+    return true;
+}
+
+bool Function::ProcessJSON_(JSON::Object::Ptr& json_result)
+{
+    // Process actions of the function
+    for(auto& action : actions_)
+    {
+        // Process Action
+        if(!ProcessAction_(action))
+        {
+            error_ = true;
+            return false;
+        }
+
+        // Set JSON results
+        if(action->get_final())
+            json_result = action->get_json_result();
+    }
+
+    return true;
+}
+
+bool Function::ProcessFile_(std::string& filepath)
+{
+    // Process actions of the function
+    for(auto& action : actions_)
+    {
+        // Process Action
+        if(!ProcessAction_(action))
+        {
+            error_ = true;
+            return false;
+        }
+
+
+        // Set filepath
+        if(action->get_final())
+        {
+            auto field = action->get_results()->First_();
+            if(!field->IsNull_())
+                filepath = field->String_();
+        }
+    }
+
+    return true;
+}
+
+void Function::DownloadProcess_(std::string& filepath)
+{
+    // Manage the file
+    file_manager_->set_operation_type(Files::OperationType::kDownload);
+    file_manager_->get_files().push_back(file_manager_->CreateTempFile_("/" + filepath));
+    auto& tmp_file = file_manager_->get_files().front();
+
+    // Check file
+    if(!file_manager_->CheckFiles_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
+        return;
+    }
+
+    // Is supported
+    if(!file_manager_->IsSupported_(tmp_file))
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
+        return;
+    }
+    file_manager_->ProcessContentLength_();
+
+    // Reponse
+    auto& response = get_http_server_response().value();
+    response->setStatus(HTTPResponse::HTTP_OK);
+    response->setContentType(tmp_file.get_content_type());
+    response->setContentLength(tmp_file.get_content_length());
+    
+    std::ostream& out_reponse = response->send();
+
+    // Download file
+    file_manager_->DownloadFile_(out_reponse);
+}
+
+void Function::UploadProcess_()
+{
+    // Manage the files
+    file_manager_->set_operation_type(Files::OperationType::kUpload);
+
+    // Change path
+    for(auto& file : file_manager_->get_files())
+    {
+        if(!file_manager_->ChangePathAndFilename_(file, file_manager_->get_directory_base()))
+        {
+            HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The file cannot be processed.");
+            return;
+        }
+    }
+
+    // Verify supported files
+    if(!file_manager_->IsSupported_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
+        return;
+    }
+
+    // Verify max file size
+    if(!file_manager_->VerifyMaxFileSize_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The requested file exceeds the file size limit.");
+        return;
+    }
+
+    // Upload file
+    file_manager_->UploadFile_();
+
+    // Response
+    CompoundFillResponse_(HTTP::Status::kHTTP_OK, file_manager_->get_result(), "Ok.");
+}
+
+void Function::ModifyProcess_(std::string& filepath)
+{
+    if(remove_file_on_modify_)
+    {
+        // Manage the file
+        Files::FileManager tmp_file_manager = Files::FileManager(*file_manager_);
+        tmp_file_manager.set_operation_type(Files::OperationType::kDelete);
+
+        tmp_file_manager.get_files().clear();
+        tmp_file_manager.get_files().push_back(file_manager_->CreateTempFile_("/" + filepath));
+
+        // Check file
+        if(!tmp_file_manager.CheckFiles_())
+        {
+            HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
+            return;
+        }
+
+        // Remove the file
+        tmp_file_manager.RemoveFile_();
+    }
+
+    // Process new file to upload
+    file_manager_->set_operation_type(Files::OperationType::kUpload);
+    
+    // Change path
+    for(auto& file : file_manager_->get_files())
+    {
+        if(!file_manager_->ChangePathAndFilename_(file, file_manager_->get_directory_base()))
+        {
+            HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The file cannot be processed.");
+            return;
+        }
+    }
+
+    // Is supported
+    if(!file_manager_->IsSupported_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
+        return;
+    }
+    
+    // Verify max file size
+    if(!file_manager_->VerifyMaxFileSize_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The requested file exceeds the file size limit.");
+        return;
+    }
+
+    // Upload
+    file_manager_->UploadFile_();
+
+    // Response
+    CompoundFillResponse_(HTTP::Status::kHTTP_OK, file_manager_->get_result(), "Ok.");
+}
+
+void Function::RemoveProcess_(std::string& filepath)
+{
+    // Manage file
+    file_manager_->set_operation_type(Files::OperationType::kDelete);
+    file_manager_->get_files().push_back(file_manager_->CreateTempFile_("/" + filepath));
+
+    // Basic operations
+    if(!file_manager_->CheckFiles_())
+    {
+        HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
+        return;
+    }
+
+    // Remove
+    file_manager_->RemoveFile_();
+        
+    // Response
+    JSONResponse_(HTTP::Status::kHTTP_OK, "Ok.");
+}
+
+void Function::SetupCustomProcess_(std::function<void(Function&)> custom_process)
+{
+    custom_process_ = custom_process;
+}
+
+void Function::IdentifyParameters_(Functions::Action::Ptr action)
+{
+    // Iterate over action parameters
+    for(auto it : action->get_parameters())
+    {
+        // Iterate over Function parameters
+        for(auto it2 : parameters_)
+        {
+            if(it2->get_name() == it->get_name() && it->get_editable())
+            {
+                // Copy function parameter value to action parameter value (Shared)
+                it->set_value(it2->get_value());
+            }
+        }
+    }
+}
---
+++ src/functions/function.h
@@ -0,0 +1,119 @@
+
+#ifndef NAF_FUNCTIONS_FUNCTION
+#define NAF_FUNCTIONS_FUNCTION
+
+
+#include <functional>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+
+#include "files/file_manager.h"
+#include "functions/action.h"
+#include "http/methods.h"
+#include "query/parameter.h"
+#include "http/request.h"
+#include "http/common_responses.h"
+#include "security/user.h"
+#include "security/users_manager.h"
+
+
+namespace NAF
+{
+    namespace Functions
+    {
+        class Function;
+    }
+}
+
+
+class NAF::Functions::Function : public HTTP::CommonResponses
+{
+    public:
+        using Ptr = std::shared_ptr<Functions::Function>;
+
+        enum class ResponseType
+        {
+            kJSON
+            ,kFile
+            ,kCustom
+        };
+
+        Function();
+        Function(std::string endpoint, HTTP::EnumMethods method, ResponseType response_type = ResponseType::kJSON);
+
+        std::string get_endpoint() const { return endpoint_; }
+        std::string get_target() const { return target_; }
+        bool get_error() const { return error_; }
+        std::string get_error_message() const { return error_message_; }
+        bool get_remove_file_on_modify() const { return remove_file_on_modify_; }
+        ResponseType get_response_type() const { return response_type_; }
+        HTTP::EnumMethods get_method() const { return method_; }
+        Security::User& get_current_user()
+        {
+            auto& var = current_user_;
+            return var;
+        }
+        std::vector<Action::Ptr>& get_actions()
+        {
+            auto& var = actions_;
+            return var;
+        }
+        HTTP::Methods& get_methods()
+        {
+            auto& var = methods_;
+            return var;
+        }
+        Files::FileManager::Ptr& get_file_manager()
+        {
+            auto& var = file_manager_;
+            return var;
+        }
+        std::vector<Query::Parameter::Ptr>& get_parameters()
+        {
+            auto& var = parameters_;
+            return var;
+        }
+
+        void set_endpoint(std::string endpoint) { endpoint_ = endpoint; }
+        void set_target(std::string target) { target_ = target; }
+        void set_remove_file_on_modify(bool remove_file_on_modify) { remove_file_on_modify_ = remove_file_on_modify; }
+        void set_method(HTTP::EnumMethods type) { method_ = type; }
+        void set_response_type(ResponseType response_type) { response_type_ = response_type; }
+
+        std::vector<Query::Parameter::Ptr>::iterator  GetParameter_(std::string name);
+        Action::Ptr AddAction_(std::string identifier);
+        std::vector<Action::Ptr>::iterator GetAction_(std::string identifier);
+        void Process_(HTTP::Request::HTTPServerRequestPtr request, HTTP::Request::HTTPServerResponsePtr response);
+        bool ProcessAction_(Action::Ptr action);
+        bool ProcessJSON_(JSON::Object::Ptr& json_result);
+        bool ProcessFile_(std::string& filepath);
+        void DownloadProcess_(std::string& filepath);
+        void UploadProcess_();
+        void ModifyProcess_(std::string& filepath);
+        void RemoveProcess_(std::string& filepath);
+        void SetupCustomProcess_(std::function<void(Function&)> custom_process);
+        void IdentifyParameters_(Functions::Action::Ptr action);
+
+    protected:
+        void Setup_(HTTP::Request::HTTPServerRequestPtr request, HTTP::Request::HTTPServerResponsePtr response);
+
+    private:
+        std::string endpoint_;
+        std::string target_;
+        bool error_;
+        std::string error_message_;
+        bool remove_file_on_modify_;
+        ResponseType response_type_;
+        HTTP::EnumMethods method_;
+        Security::User current_user_;
+        std::vector<Action::Ptr> actions_;
+        HTTP::Methods methods_;
+        Files::FileManager::Ptr file_manager_;
+        std::function<void(Function&)> custom_process_;
+        std::vector<Query::Parameter::Ptr> parameters_;
+};
+
+
+#endif // NAF_FUNCTIONS_FUNCTION
