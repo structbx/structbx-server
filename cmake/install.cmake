7a268cfe668ef5e4b905254a9661dc24345a7816
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 18:02:31 -0400

    chore: Add CMake install cert and key

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -9,3 +9,11 @@ install(
 	FILES ${PROJECT_SOURCE_DIR}/conf/properties.yaml.template
 	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/properties.yaml
 )
+install(
+	FILES ${PROJECT_SOURCE_DIR}/conf/cert.pem.template
+	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/cert.pem
+)
+install(
+	FILES ${PROJECT_SOURCE_DIR}/conf/key.pem.template
+	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/key.pem
+)
