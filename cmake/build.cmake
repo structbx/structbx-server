b36d0b993837e3a7f53ef580f7bd69ebd9d147b8
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 15:02:52 -0400

    chore: Add build.cmake
    Rel: #40

---
+++ cmake/build.cmake
@@ -0,0 +1,35 @@
+# Build security
+if(PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
+	message(
+		FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there."
+	)
+endif()
+
+# Language standard
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+# Build type
+if(NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE Release)
+endif()
+
+# Compiler options
+set(CMAKE_CXX_FLAGS_DEBUG "-g -O0 -Wall -Wextra -fstandalone-debug")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3")
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Module and Prefix Path
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
+list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+
+# Setting up header
+set(HEADER ${PROJECT_SOURCE_DIR}/src)
+list(APPEND HEADER ${PROJECT_BINARY_DIR})
+
+# Copy properties file
+configure_file(
+    ${CMAKE_SOURCE_DIR}/conf/properties.yaml.template
+    ${CMAKE_BINARY_DIR}/properties.yaml
+  COPYONLY
+)
