7d9dce33f666f7578030ab2c7676c55882c9f857
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 16:00:47 -0400

    refactor: Eliminate documentation and correct namespaces in http
    Rel: #41, #42

--- src/http/methods.h
+++ src/http/methods.h
@@ -1,23 +1,6 @@
-/*
-* <one line to give the program's name and a brief idea of what it does.>
-* Copyright (C) 2021  <copyright holder> <email>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
-#ifndef STRUCTBX_HTTPMETHODS
-#define STRUCTBX_HTTPMETHODS
+#ifndef STRUCTBX_HTTP_METHODS
+#define STRUCTBX_HTTP_METHODS
 
 #include <map>
 #include <string>
@@ -96,4 +79,4 @@ class StructBX::HTTP::Methods
         std::list<MethodStruct> process_;
 };
 
-#endif // STRUCTBX_HTTPMETHODS
+#endif // STRUCTBX_HTTP_METHODS
