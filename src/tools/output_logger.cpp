cf69502dd2b51b0f485545a92cfe762bb5a19a51
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 16:01:56 -0400

    refactor: Eliminate documentation and correct namespaces in tools
    Rel: #41, #42

--- src/tools/output_logger.cpp
+++ src/tools/output_logger.cpp
@@ -1,20 +1,3 @@
-/*
-* <one line to give the program's name and a brief idea of what it does.>
-* Copyright (C) 2021  <copyright holder> <email>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include "tools/output_logger.h"
 
@@ -23,7 +6,7 @@ using namespace StructBX::Tools;
 std::mutex OutputLogger::mutex_;
 bool OutputLogger::log_to_file_ = false;
 bool OutputLogger::print_debug_ = false;
-std::string OutputLogger::output_file_address_ = "nebulaatom.log";
+std::string OutputLogger::output_file_address_ = "structbx.log";
 
 OutputLogger::OutputLogger()
 {
