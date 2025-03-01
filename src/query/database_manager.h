79213e39f7a13c5bb90bdecb7941a75ac32cb342
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 16:01:01 -0400

    refactor: Eliminate documentation and correct namespaces in query
    Rel: #41, #42

--- src/query/database_manager.h
+++ src/query/database_manager.h
@@ -1,23 +1,6 @@
-/*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2023  Jose F Rivas C <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
-#ifndef STRUCTBX_QUERY_DATABASE_MANAGER
-#define STRUCTBX_QUERY_DATABASE_MANAGER
+#ifndef STRUCTBX_QUERY_DATABASEMANAGER
+#define STRUCTBX_QUERY_DATABASEMANAGER
 
 
 #include <iostream>
@@ -91,4 +74,4 @@ class StructBX::Query::DatabaseManager
 
 };
 
-#endif // STRUCTBX_QUERY_DATABASE_MANAGER
+#endif // STRUCTBX_QUERY_DATABASEMANAGER
