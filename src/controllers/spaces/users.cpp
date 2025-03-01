2c958899ca438d1761c0a1cb68f0f454e8c1c4db
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-01 17:14:58 -0400

    feat: Move Controllers::Spaces
    Rel: #43

---
+++ src/controllers/spaces/users.cpp
@@ -0,0 +1,155 @@
+
+#include "controllers/spaces/users.h"
+
+using namespace StructBX::Controllers::Spaces;
+
+Users::Users(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+    ,struct_read_(function_data)
+    ,struct_read_user_out_space_(function_data)
+    ,struct_add_(function_data)
+    ,struct_delete_(function_data)
+{
+    
+}
+
+Users::Read::Read(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+{
+    // Function GET /api/spaces/users/read
+    StructBX::Functions::Function::Ptr function = 
+        std::make_shared<StructBX::Functions::Function>("/api/spaces/users/read", HTTP::EnumMethods::kHTTP_GET);
+    
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code(
+        "SELECT nu.id, nu.username, sp.created_at " \
+        "FROM users nu " \
+        "JOIN spaces_users sp ON sp.id_naf_user = nu.id " \
+        "WHERE sp.id_space = (SELECT id FROM spaces WHERE identifier = ?)"
+    );
+    action1->AddParameter_("identifier", "", true)
+    ->SetupCondition_("condition-id_space", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->ToString_() == "")
+        {
+            param->set_error("El identificador de espacio no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+
+    get_functions()->push_back(function);
+}
+
+Users::ReadUserOutSpace::ReadUserOutSpace(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+{
+    // Function GET /api/spaces/users/out/read
+    StructBX::Functions::Function::Ptr function = 
+        std::make_shared<StructBX::Functions::Function>("/api/spaces/users/out/read", HTTP::EnumMethods::kHTTP_GET);
+    
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code(
+        "SELECT nu.id, nu.username "
+        "FROM users nu "
+        "LEFT JOIN spaces_users su ON "
+            "su.id_naf_user = nu.id AND "
+            "su.id_space = (SELECT s.id FROM spaces s JOIN spaces_users su2 ON su2.id_space = s.id WHERE identifier = ? AND su2.id_naf_user = ? LIMIT 1) "
+        "WHERE "
+            "su.id_naf_user IS NULL "
+    );
+    action1->AddParameter_("identifier_space", "", true)
+    ->SetupCondition_("condition-identifier_space", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->ToString_() == "")
+        {
+            param->set_error("El identificador de espacio no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+    action1->AddParameter_("id_user", get_id_user(), false);
+
+    get_functions()->push_back(function);
+}
+
+Users::Add::Add(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+{
+    // Function GET /api/spaces/users/add
+    StructBX::Functions::Function::Ptr function = 
+        std::make_shared<StructBX::Functions::Function>("/api/spaces/users/add", HTTP::EnumMethods::kHTTP_POST);
+    
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code(
+        "INSERT INTO spaces_users (id_space, id_naf_user) "
+        "SELECT "
+            "(SELECT id FROM spaces WHERE identifier = ?) "
+            ",? "
+    );
+    action1->AddParameter_("identifier_space", "", true)
+    ->SetupCondition_("condition-identifier_space", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->ToString_() == "")
+        {
+            param->set_error("El identificador de espacio no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+    action1->AddParameter_("id_user", "", true)
+    ->SetupCondition_("condition-id_user", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->ToString_() == "")
+        {
+            param->set_error("El id de usuario no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+
+    get_functions()->push_back(function);
+}
+
+Users::Delete::Delete(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+{
+    // Function GET /api/spaces/users/delete
+    StructBX::Functions::Function::Ptr function = 
+        std::make_shared<StructBX::Functions::Function>("/api/spaces/users/delete", HTTP::EnumMethods::kHTTP_DEL);
+    
+    auto action1 = function->AddAction_("a1");
+    action1->set_sql_code(
+        "DELETE su FROM spaces_users su "
+        "JOIN organizations_users ou ON ou.id_naf_user = su.id_naf_user "
+        "WHERE "
+            "su.id_naf_user = ? "
+            "AND su.id_space = ( "
+                "SELECT s.id "
+                "FROM spaces s JOIN spaces_users su ON su.id_space = s.id "
+                "WHERE s.identifier = ? AND su.id_naf_user = ? LIMIT 1)"
+    );
+    action1->AddParameter_("id", "", true)
+    ->SetupCondition_("condition-id", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->get_value()->ToString_() == "")
+        {
+            param->set_error("El id de usuario no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+    action1->AddParameter_("space_identifier", "", true)
+    ->SetupCondition_("condition-space_identifier", Query::ConditionType::kError, [](Query::Parameter::Ptr param)
+    {
+        if(param->get_value()->ToString_() == "")
+        {
+            param->set_error("El identificador de espacio no puede estar vacío");
+            return false;
+        }
+        return true;
+    });
+    action1->AddParameter_("id_user", get_id_user(), false);
+
+    get_functions()->push_back(function);
+}
---
+++ src/controllers/spaces/users.h
@@ -0,0 +1,52 @@
+
+#ifndef STRUCTBX_CONTROLLERS_SPACES_USERS_H
+#define STRUCTBX_CONTROLLERS_SPACES_USERS_H
+
+
+#include "tools/base_action.h"
+#include "tools/function_data.h"
+
+namespace StructBX
+{
+    namespace Controllers
+    {
+        namespace Spaces
+        {
+            class Users;
+        }
+    }
+}
+
+using namespace StructBX;
+
+class StructBX::Controllers::Spaces::Users : public Tools::FunctionData
+{
+    public:
+        Users(Tools::FunctionData& function_data);
+        
+    protected:
+        struct Read : public Tools::FunctionData
+        {
+            Read(Tools::FunctionData& function_data);
+        };
+        struct ReadUserOutSpace : public Tools::FunctionData
+        {
+            ReadUserOutSpace(Tools::FunctionData& function_data);
+        };
+        struct Add : public Tools::FunctionData
+        {
+            Add(Tools::FunctionData& function_data);
+        };
+        struct Delete : public Tools::FunctionData
+        {
+            Delete(Tools::FunctionData& function_data);
+        };
+
+    private:
+        Read struct_read_;
+        ReadUserOutSpace struct_read_user_out_space_;
+        Add struct_add_;
+        Delete struct_delete_;
+};
+
+#endif //STRUCTBX_CONTROLLERS_SPACES_USERS_H
