9c9dc14db360a06e905f6e630fad103e3cc0cfed
Jose F Rivas C <josefelixrc7@gmail.com>
2025-11-02 00:14:48 -0400

    chore: Improve CMake install sentences

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,19 +1,39 @@
 
 # Install files
 include(GNUInstallDirs)
+
+# Create config directory
+set(CONFIG_OUTPUT_DIR "${CMAKE_BINARY_DIR}/config_files")
+file(MAKE_DIRECTORY "${CONFIG_OUTPUT_DIR}")
+
+## a) properties.yaml
+configure_file(
+    "${PROJECT_SOURCE_DIR}/conf/properties.yaml.template"
+    "${CONFIG_OUTPUT_DIR}/properties.yaml"
+    COPYONLY
+)
+## b) cert.pem
+configure_file(
+    "${PROJECT_SOURCE_DIR}/conf/cert.pem.template"
+    "${CONFIG_OUTPUT_DIR}/cert.pem"
+    COPYONLY
+)
+# c) key.pem
+configure_file(
+    "${PROJECT_SOURCE_DIR}/conf/key.pem.template"
+    "${CONFIG_OUTPUT_DIR}/key.pem"
+    COPYONLY
+)
+
+# Install files
 install(
     TARGETS structbx-server
     DESTINATION ${CMAKE_INSTALL_BINDIR}
 )
 install(
-	FILES ${PROJECT_SOURCE_DIR}/conf/properties.yaml.template
-	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/properties.yaml
-)
-install(
-	FILES ${PROJECT_SOURCE_DIR}/conf/cert.pem.template
-	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/cert.pem
-)
-install(
-	FILES ${PROJECT_SOURCE_DIR}/conf/key.pem.template
-	DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx/key.pem
+    FILES
+        "${CONFIG_OUTPUT_DIR}/properties.yaml"
+        "${CONFIG_OUTPUT_DIR}/cert.pem"
+        "${CONFIG_OUTPUT_DIR}/key.pem"
+    DESTINATION ${CMAKE_INSTALL_SYSCONFDIR}/structbx
 )
